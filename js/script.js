aafac761c4b57a991c85898f4b98d2e0e0dc072c
Ryeker <ryekerjh@gmail.com>
2017-10-23 17:35:16 -0700

    next topic

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ function queryRandomUserAPI() {
         // This is where you run code if the server returns any errors
     });
 };
+
+
+
+//slightly more complicated example using same structure
+function queryWithParams(queryCategory, queryTerm) {
+  //How can we dynamically get queryCategory and query?
+  
+  const url =`https://randomuser.me/api/?${queryCategory}=${queryTerm}&results=10`;
+  fetch(url)
+  .then(function(resp){
+    return resp.json()// Transform the data into json
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+}
